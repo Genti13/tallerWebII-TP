c1f04e3c86b941bef7c4181644605dd4b298c7ea
Magali <maga.bjb@gmail.com>
2025-06-14 20:43:35 -0300

    funcionamiento login y registro

--- src/app/servicios/ProductService/producto.service.ts
+++ src/app/servicios/ProductService/producto.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductoService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = 'http://localhost:4200';
 
   constructor(private http: HttpClient) {}
 
